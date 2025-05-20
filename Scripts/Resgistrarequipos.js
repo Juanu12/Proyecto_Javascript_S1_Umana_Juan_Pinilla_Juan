31507144fa88c7e04b8c01c1c74c594d1d74b49a
JuanPinilla16 <juanepinilla16@gmail.com>
2025-05-20 09:36:16 -0500

    feat: :sparkles: uptadatin html info

--- Scripts/Resgistrarequipos.js
+++ Scripts/Resgistrarequipos.js
@@ -1,13 +1,45 @@
 async function Fetchdata() {
-try{ 
-        response = await fetch("")
-        data = response.json()
+        try {
+                response = await fetch("https://6818a2f25a4b07b9d1d018e3.mockapi.io/Equipos ")
+                if (!response.ok) {
+                        throw new Error(`Error HTTP: ${response.status}`)
+                }
+                data = response.json()
 
-        return data
-    }
-    
-catch(error){
+                return data
+        }
+
+        catch (error) {
+                console.error("Ocurrió un error",error)
 
         }
-    
+
+}
+
+
+async function Mostrarequipos(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Button = document.getElementById("Send").addEventListener("click" , async function (e) {
+e.preventDefault()
+
+        
+})
+
+
+
+
 }
--- Scripts/Verequipos.js
+++ Scripts/Verequipos.js
@@ -1,13 +1,17 @@
 async function Fetchdata() {
-try{ 
-        response = await fetch("")
-        data = response.json()
+        try {
+                response = await fetch("https://6818a2f25a4b07b9d1d018e3.mockapi.io/Equipos ")
+                if (!response.ok) {
+                        throw new Error(`Error HTTP: ${response.status}`)
+                }
+                data = response.json()
 
-        return data
-    }
-    
-catch(error){
+                return data
+        }
+
+        catch (error) {
+                console.error("Ocurrió un error",error)
 
         }
-    
+
 }
