b772c912fc3e0876d6a7a1f211b299f66d8a3601
Juanu12 <juanferumana@hotmail.com>
2025-05-21 10:04:47 -0500

    feat: :sparkles: Improving code

--- Scripts/Circuits.js
+++ Scripts/Circuits.js
@@ -1,12 +1,17 @@
+
+circuits = document.getElementById("Circuitsjs")
+
+
+
 async function Fetchdata(){
     try{
-        const response = await fetch("")
+        const response = await fetch("https://6813e31f225ff1af16276aea.mockapi.io/api/v1/students/Circuits")
         if (!response.ok){
             throw new Error(`Error HTTP: ${response.status}`);
         }
         
         
-       const data = await reponse.json()
+       const data = await response.json()
         console.log(data)
         return data
     
@@ -19,3 +24,33 @@ async function Fetchdata(){
     
     
     }
+
+
+    async function Showdata(){
+            circuits = document.getElementById("Circuitsjs")
+            data = await Fetchdata()
+            let  content= ""
+            for (i=0; i<data.length; i++){
+                let info = data[i]
+
+
+                content += `   
+             
+        
+        
+        
+                <div class="Circuitscontainer_elements">
+                    <img src="${info.imagen} alt="${info.imagen} ">
+                    <p>dshfgkljdfglhkdfhkd</p>
+                
+                </div>
+                
+        
+        
+            `
+            }
+
+            circuits.innerHTML = content
+    }
+
+    Showdata()
